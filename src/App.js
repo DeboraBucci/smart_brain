e13b66d0195700c333414d4f10b8b551cb46486c
Débora Bucci <dbucci@protonmail.com>
2021-12-30 17:09:24 -0300

    adding clarifai to allow face recognition app to be used

--- src/App.js
+++ src/App.js
@@ -81,6 +81,21 @@ const opt = {
 };
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: "",
+    };
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  };
+
+  onButtonSubmit = () => {
+    console.log("click");
+  };
+
   render() {
     return (
       <div className="App">
@@ -88,7 +103,10 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onSubmit={this.onButtonSubmit}
+        />
       </div>
     );
   }
