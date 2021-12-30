35fc92804001ac90b234e7789297d48025bd5b9d
Débora Bucci <dbucci@protonmail.com>
2021-12-30 03:04:03 -0300

    add Tilt component and modify it by making it smaller, without background, and with some of the classes from "tachyons", and delete unnecesary code

--- src/components/logo/Logo.js
+++ src/components/logo/Logo.js
@@ -1,7 +1,20 @@
 import React from "react";
+import Tilt from "react-parallax-tilt";
 
 const Logo = () => {
-  return <div></div>;
+  return (
+    <div className="ma4 mt0">
+      <Tilt
+        className="Tilt br2 shadow-2"
+        style={{
+          height: "150px",
+          width: "150px",
+        }}
+      >
+        <div></div>
+      </Tilt>
+    </div>
+  );
 };
 
 export default Logo;
