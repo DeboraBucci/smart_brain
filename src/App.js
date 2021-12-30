5b7ef59e63b1891c2d93884f0222529b51dc8ab1
Débora Bucci <dbucci@protonmail.com>
2021-12-30 02:52:13 -0300

    create a 'logo' folder inside the components folder, and there, create a 'Logo.js' component inside, then import and use it in the 'App.js' file

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import React, { Component } from "react";
 
 import Navigation from "./components/navigation/Navigation";
+import Logo from "./components/logo/Logo";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navigation />
+        <Logo />
       </div>
     );
   }
