c56a1ee03e2bd9cdf06d7ab4a653a6b84a730b06
Débora Bucci <dbucci@protonmail.com>
2021-12-30 02:38:12 -0300

    create a folder named 'components' and inside of it create a folder named 'navigation' that has a 'Navigation.js' file inside, then import  and add to 'App.js' that same file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import "./App.css";
 import React, { Component } from "react";
 
+import Navigation from "./components/navigation/Navigation";
+
 class App extends Component {
   render() {
-    return <div className="App"></div>;
+    return (
+      <div className="App">
+        <Navigation />
+      </div>
+    );
   }
 }
 
