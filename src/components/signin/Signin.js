3849e92e885c543cdcf67dfdd11f133d86d076eb
Débora Bucci <dbucci@protonmail.com>
2022-01-03 11:46:34 -0300

    change the register link from an <a> element to a <p> one, and its coursor to pointer

--- src/components/signin/Signin.js
+++ src/components/signin/Signin.js
@@ -42,9 +42,13 @@ const Signin = ({ onRouteChange }) => {
             />
           </div>
           <div className="lh-copy mt3">
-            <a href="#0" className="f6 link dim black db">
+            <p
+              onClick={() => onRouteChange("register")}
+              href="#0"
+              className="f6 link dim black db pointer"
+            >
               Register
-            </a>
+            </p>
           </div>
         </form>
       </main>
