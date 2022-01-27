d0918cfb3c4fa12a3f393d9cd655df75a15ad944
Debora Bucci <dbucci@protonmail.com>
2022-01-27 08:57:27 -0300

    gather the information into a json object  when the register button is clicked and send it to the server in the localhost port 3001 by a post request

--- src/components/register/Register.js
+++ src/components/register/Register.js
@@ -22,8 +22,20 @@ class Register extends React.Component {
     this.setState({ password: event.target.value });
   };
 
+  onSubmitSignIn = () => {
+    fetch("http://localhost:3001/register", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        email: this.state.email,
+        password: this.state.password,
+        name: this.state.name,
+      }),
+    });
+  };
+
   render() {
-    const { onRouteChange, onNameChange, onEmailChange, onPasswordChange } =
+    const { onSubmitSignIn, onNameChange, onEmailChange, onPasswordChange } =
       this.props;
     return (
       <article
@@ -71,7 +83,7 @@ class Register extends React.Component {
             </fieldset>
             <div className="">
               <input
-                onClick={() => onRouteChange("home")}
+                onClick={onSubmitSignIn}
                 className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
                 type="submit"
                 value="Register"
